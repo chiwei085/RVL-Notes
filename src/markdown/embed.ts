99c83d8dff7d113b59dbfbfa7a47a4b5e2de3a01
chiwei085 <yehchiwei.work@gmail.com>
2026-03-01 21:52:47 +0800

    [chore] 移除 markdown 舊版殘留插件

--- src/markdown/embed.ts
+++
@@ -1,8 +0,0 @@
-import type { Plugin } from "unified";
-
-// Replaced by micromark syntax extension.
-export function createEmbedPlugin(): Plugin<[], any> {
-  return () => {
-    return;
-  };
-}
--- src/markdown/kbd.ts
+++
@@ -1,8 +0,0 @@
-import type { Plugin } from "unified";
-
-// Replaced by micromark syntax extension.
-export function createKbdPlugin(): Plugin<[], any> {
-  return () => {
-    return;
-  };
-}
